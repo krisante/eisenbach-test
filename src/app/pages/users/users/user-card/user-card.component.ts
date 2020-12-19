4c92d5f0d5273221211928bc82a5d3daacd11846
Krisante Timol <timol.krisante@gmail.com>
2020-12-19 18:11:12 +0800

    add some simple styling

--- src/app/pages/users/users/user-card/user-card.component.ts
+++ src/app/pages/users/users/user-card/user-card.component.ts
@@ -6,7 +6,7 @@ import { UserDetails } from 'src/app/core/models/users.model';
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.scss'],
   host: {
-    class: 'rounded shadow d-block container py-3 my-3 d-flex flex-column align-items-center'
+    class: 'rounded border d-block container py-3 my-3 d-flex flex-column align-items-center bg-dark'
   }
 })
 export class UserCardComponent implements OnInit {
